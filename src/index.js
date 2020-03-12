22b8fb699e418ddd691ff691a411168b6a9705ca
Niya Anil <niyapanil@gmail.com>
2020-03-11 23:37:59 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import reactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, NavLink, Redirect} from 'react-router-dom';
+import RegisterBox from './register';
+import './login.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class LoginBox extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    
+    render() {
+      return (
+        <Router>
+        <div className="root-container">
+        <div className="box-container">
+            
+        <div className="inner-container">
+          <div className="header">
+            Login
+          </div>
+          <div className="box">
+  
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                className="login-input"
+                placeholder="Username"/>
+            </div>
+  
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="login-input"
+                placeholder="Password"/>
+            </div>
+  
+            <button
+              type="button"
+              className="login-btn"
+            >Login</button>
+          </div>
+          <p>Haven't yet registered?? <NavLink to={'/register'} className="nav-link" > Register </NavLink></p>
+        </div>
+     
+        
+         
+        
+        </div>
+
+        </div>
+        </Router>
+      );
+    }
+  
+  }
+
+reactDOM.render(
+  <LoginBox/>, document.getElementById("root"));
